6150c6b88f1907c8f51682837eda68539cff40f8
Pat Heard <patrick.heard@tpsgc-pwgsc.gc.ca>
2019-06-06 22:33:59 -0400

    Setup node project and add webpack build
    All dependencies except for Google Material Icons are now
    local to the project and built into a bundle.js/bundle.css.

---
+++ prototype/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './css/app.scss',
+    './js/app.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Pipes the extracted CSS into a bundle.css file
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            }
+          },
+          { 
+            // Extracts the Bootstrap CSS
+            loader: 'extract-loader' 
+          },    
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // Only load the English/French locales for Chart.js
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en|fr/)
+  ]  
+};
